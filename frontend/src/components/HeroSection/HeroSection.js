3fb4ee2c1b679f5a99686fcb28500bb8e0e0329b
Amyzi <ebiojoidris@icloud.com>
2024-09-09 22:31:21 +0200

    all

---
+++ frontend/src/components/HeroSection/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <div className="landing-page-hero">
+      <div className="hero-content">
+        <h1>4Life</h1>
+        <strong>Find Your Path. Achieve Your Goals.</strong>
+        <div>
+          <button>Find A Mentor</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <HeroSection />
+      {/* Add more content for the landing page */}
+    </div>
+  );
+};
+
+export default LandingPage;
