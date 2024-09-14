e6a9e531a98b29164191c0859d09c4f82ed6439a
Young-proflat <olatunbosunlateef6@gmail.com>
2024-09-14 03:07:50 -0700

    4Life-Backend

---
+++ 4lifeRe/Backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, flask_wtform, flash, url_for, redirect
+from flask render_template
+from flask_bcrypt import Bcrypt, check_password_hash
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+
+
+
+
+
+
+
+
+
+
+
+
+
